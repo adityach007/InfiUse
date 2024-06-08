ebcad317cc776c88d722c251a30de06548981130
ADITYA CHOUDHARY <108794914+adityach007@users.noreply.github.com>
2024-06-08 21:40:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -268,8 +268,6 @@ def main():
     if 'user_key' not in st.session_state:
         login_interface()
     else:
-        
-        st.sidebar.image("C:/Users/ashup/OneDrive/Desktop/GROQ/icon1.png")
         st.sidebar.title(f"Welcome {st.session_state['username']}")
 
 
